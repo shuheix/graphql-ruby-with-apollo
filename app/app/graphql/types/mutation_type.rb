0d60963fbf6a7eda6d3b9767d3568d7788d30401
shuheix <ono.shuhei210@gmail.com>
2025-04-13 22:06:25 +0900

    sample_mutation

--- app/app/graphql/types/mutation_type.rb
+++ app/app/graphql/types/mutation_type.rb
@@ -1,12 +1,10 @@
 # frozen_string_literal: true
 
 module Types
+  # MutationType
   class MutationType < Types::BaseObject
-    # TODO: remove me
-    field :test_field, String, null: false,
-                               description: 'An example field added by the generator'
-    def test_field
-      'Hello World'
-    end
+    field :create_message, mutation: Mutations::CreateMessage
+    field :create_channel, mutation: Mutations::CreateChannel
+    field :create_workspace, mutation: Mutations::CreateWorkspace
   end
 end
